63730efae9099f2ce7aa90d8dd13dc18ac091df3
saurabhkumar-sk <saurabhkumakairi501@gmail.com>
2023-10-30 02:48:55 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:my_website/my_home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Portfolio Website',
+      theme: ThemeData(
+        fontFamily: 'Exo',
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LandingScreen(),
+    );
+  }
+}
